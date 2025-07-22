03991287db31a5b40ae396be5e10973ce8ea545e
Maybe <1913093102@qq.com>
2025-07-22 18:15:38 +0800

    ann

--- src/renderer/tests/components/TestPage.test.ts
+++ src/renderer/tests/components/TestPage.test.ts
@@ -1,8 +1,16 @@
+/*
+ * @Author: Maybe 1913093102@qq.com
+ * @Date: 2025-07-21 13:04:49
+ * @LastEditors: Maybe 1913093102@qq.com
+ * @LastEditTime: 2025-07-22 18:12:34
+ * @FilePath: \EleTs\src\renderer\tests\components\TestPage.test.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import TestPage from '@renderer/view/TestPage/TestPage.vue'
 
-// Mock 子组件
+// Mock 子组件 - 模拟 TestPage 组件中使用的子组件，避免测试时加载真实组件
 vi.mock('@renderer/view/Components/Button1.vue', () => ({
   default: { template: '<button><slot></slot></button>' }
 }))
