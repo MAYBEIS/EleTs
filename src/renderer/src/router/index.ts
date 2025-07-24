aef94b3f8be23e12d5857b8705e0b95c679de517
Maybe <1913093102@qq.com>
2025-07-24 11:30:11 +0800

    del router

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: Maybe 1913093102@qq.com
  * @Date: 2025-01-12 12:13:32
  * @LastEditors: Maybe 1913093102@qq.com
- * @LastEditTime: 2025-07-21 15:52:30
+ * @LastEditTime: 2025-07-24 11:30:02
  * @FilePath: \EleTs\src\renderer\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,11 +13,6 @@ const routes = [
   {
     path: '/',
     redirect: '/index'
-  },
-  {
-    path: '/index',
-    name: 'index',
-    component: (): Promise<Component> => import('../view/App/MainWin.vue')
   },
 ]
 
