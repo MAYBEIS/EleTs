5901b46f503b2eeb72e5ae9cb07e3a033588019b
Maybe <1913093102@qq.com>
2025-07-24 09:42:33 +0800

    del

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: Maybe 1913093102@qq.com
  * @Date: 2024-12-08 15:34:41
  * @LastEditors: Maybe 1913093102@qq.com
- * @LastEditTime: 2025-07-23 09:50:25
+ * @LastEditTime: 2025-07-24 09:42:27
  * @FilePath: \EleTs\src\renderer\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -17,7 +17,6 @@ import HomePage from './view/HomePage/HomePage.vue';
     <!-- <div class="AppAll flex scroll-hidden "> -->
     <!-- 调试模式，窗口背景铺满 -->
     <div class="AppAll h-full w-full flex scroll-hidden bg-red-200">
-      <!-- <mainWin id="Index-win" class="h-full w-full overflow-hidden drag_able"></mainWin> -->
       <!-- <HomePage id="Index-win" class="h-full w-full overflow-hidden drag_able"></HomePage> -->
       <TestPage id="Index-win" class="h-full w-full overflow-hidden drag_able "></TestPage>
     </div>
