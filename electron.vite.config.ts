8aa70afa0ef9ac3a1e23abb8ddc333be8a9a5393
Maybe <1913093102@qq.com>
2025-07-24 16:46:05 +0800

    @renderer

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: Maybe 1913093102@qq.com
  * @Date: 2024-12-08 15:34:41
  * @LastEditors: Maybe 1913093102@qq.com
- * @LastEditTime: 2025-07-21 18:50:43
+ * @LastEditTime: 2025-07-24 16:26:02
  * @FilePath: \EleTs\electron.vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,7 @@ export default defineConfig({
     resolve: {
       alias: {
         // 配置路径别名，@renderer 指向 src/renderer 目录
-        '@renderer': resolve('src/renderer')
+        '@renderer': resolve('src/renderer/src')
       }
     },
     plugins: [
@@ -34,9 +34,6 @@ export default defineConfig({
           'vue'
         ]
       }),
-      // Components({
-      //   resolvers: [NaiveUiResolver()]
-      // })
     ]
   },
 })
