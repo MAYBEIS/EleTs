7843f041da14a489e8e799be0d1dad2e2edff868
Maybe <1913093102@qq.com>
2025-08-18 22:33:42 +0800

    加密和打包

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,19 +2,19 @@
  * @Author: Maybe 1913093102@qq.com
  * @Date: 2024-12-08 15:34:41
  * @LastEditors: Maybe 1913093102@qq.com
- * @LastEditTime: 2025-07-27 21:44:08
+ * @LastEditTime: 2025-08-18 21:51:28
  * @FilePath: \EleTs\electron.vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, bytecodePlugin  } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(),bytecodePlugin()],
     resolve: {
       alias: {
         '@main': resolve('src/main'),
@@ -23,7 +23,7 @@ export default defineConfig({
     }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(),bytecodePlugin()],
     resolve: {
       alias: {
         '@main': resolve('src/main'),
