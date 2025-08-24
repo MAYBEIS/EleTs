570030630343a86687dc7a57fa70394615773464
Maybe <1913093102@qq.com>
2025-08-24 23:41:00 +0800

    bad code

--- src/renderer/src/view/LoraPage/CivitaiPage.vue
+++ src/renderer/src/view/LoraPage/CivitaiPage.vue
@@ -2,7 +2,7 @@
  * @Author: Maybe 1913093102@qq.com
  * @Date: 2025-07-21 16:28:41
  * @LastEditors: Maybe 1913093102@qq.com
- * @LastEditTime: 2025-08-24 22:42:36
+ * @LastEditTime: 2025-08-24 23:34:06
  * @FilePath: \EleTs\src\renderer\src\view\LoraPage\CivitaiPage.vue
  * @Description: Civitai模型浏览和下载页面
 -->
